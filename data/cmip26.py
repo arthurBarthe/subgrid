1c1c7fea66ef1071d3702258cb3fe0d622408e54
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-02-20 16:37:15 -0500

    Added feature: read from pangeo data catalog + compute subgrid forcing

---
+++ data/cmip26.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 15:19:16 2020
+
+@author: arthur
+This script allows to select data from the cmip2.6 simulations made 
+available on the Pangeo intake data catalog. Run cmip26 -h to display
+help.
+"""
+
+import argparse
+import dask
+from dask.diagnostics import ProgressBar
+import intake
+from convert_lat_long import *
+from coarse import *
+
+# Script parameters
+catalog_url = 'https://raw.githubusercontent.com/pangeo-data/pangeo-datastore\
+/master/intake-catalogs/master.yaml'
+description = 'Read data from the CMIP2.6 from a particular region and \
+    applies coarse graining.'
+
+
+def get_patch(params, catalog_url, *selected_vars):
+    catalog = intake.open_catalog(catalog_url)
+    
+    if params.CO2 == 0:
+        s = catalog.ocean.GFDL_CM2_6.GFDL_CM2_6_control_ocean_surface
+    else:
+        raise('Only control implemented for now.')
+    data = s.to_dask()
+    my_data = data.sel(xu_ocean=slice(*params.bounds[2:]),
+                       yu_ocean=slice(*params.bounds[:2]))
+    my_data = my_data.isel(time=slice(0, params.ntimes))
+    
+    if selected_vars is None:
+        return my_data
+    else:
+        return my_data[list(selected_vars)]
+
+
+
+if __name__ == '__main__':
+    import sys
+    # Parse the command-line parameters
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('bounds', type=float, nargs=4, 
+                        help='min lat, max_lat, min_long, max_long')
+    parser.add_argument('--ntimes', type=int, default=3000)
+    parser.add_argument('--CO2', type=int, default=0, choices=[0,1])
+    if len(sys.argv) > 1:
+        params = parser.parse_args()
+    else:
+        params = parser.parse_args('0 20 10 15'.split())
+
+    # Retrieve the patch of data specified in the command-line args
+    patch_data = get_patch(params, catalog_url, 'usurf', 'vsurf')
+    
+    # Convert to x-y coordinates
+    patch_data = latlong_to_euclidean(patch_data, 'yu_ocean', 'xu_ocean')
+    print(patch_data)
+
+    # Calculate eddy-forcing dataset for that particular patch
+    forcing = eddy_forcing(patch_data, scale=30e3, method='mean')
+    print('Done!!!')
+    pbar = ProgressBar()
+    pbar.register()
+    forcing.compute()
---
+++ data/coarse.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 12:15:35 2020
+
+@author: arthur
+"""
+
+import xarray as xr
+import dask
+import dask.array as da
+import dask.bag as db
+from scipy.ndimage import gaussian_filter
+import numpy as np
+
+def advections(u_v_dataset):
+    """Computes advection terms"""
+    gradient_x = u_v_dataset.differentiate('x')
+    gradient_y = u_v_dataset.differentiate('y')
+    u, v = u_v_dataset['usurf'], u_v_dataset['vsurf']
+    adv_x = u * gradient_x['usurf'] + v * gradient_y['usurf']
+    adv_y = v * gradient_x['vsurf'] + v * gradient_y['vsurf']
+    return xr.Dataset({'adv_x': adv_x, 'adv_y' :adv_y})
+
+
+# def spatial_filter(data, scale):
+#     print('scale', scale)
+#     result = None
+#     for time in range(data.shape[0]):
+#         print(time)
+#         gf = dask.delayed(gaussian_filter)(data[time, ...], scale)
+#         if result is None:
+#             result = dask.array.from_delayed(gf, shape=data.shape[1:],
+#                                              dtype=float)
+#         else:
+#             gf = dask.array.from_delayed(gf, shape = data.shape[1:], 
+#                                          dtype=float)
+#             result = dask.array.concatenate((result, gf))
+#     return result
+
+def spatial_filter(data, scale):
+    result = np.zeros_like(data)
+    for t in range(data.shape[0]):
+        result[t, ...] = gaussian_filter(data[t, ...], scale)
+    return result
+
+
+def eddy_forcing(u_v_dataset, scale: float, method='mean'):
+    step_x = abs(u_v_dataset.coords['x'].values.mean())
+    step_y = abs(u_v_dataset.coords['y'].values.mean())
+    print('A')
+    adv = advections(u_v_dataset)
+    print('C')
+    def ufunc(x):
+        return gaussian_filter(x, (0, scale / step_x, scale / step_y))
+    u_v_filtered = xr.apply_ufunc(lambda x: spatial_filter(x, 2), u_v_dataset, dask='parallelized', 
+                                  output_dtypes=[float,])
+    return u_v_filtered
+    print('D')
+    adv_filtered = advections(u_v_filtered)
+    print('E')
+    forcing = adv_filtered - adv
+    forcing.rename({'adv_x' : 'S_x', 'adv_y' : 'S_y'})
+    forcing = forcing.coarsen({'x': int(scale / step_x), 
+                               'y': int(scale / step_y)}, boundary='trim')
+    if method == 'mean':
+        return forcing.mean()
+    else:
+        raise('Passed method does not correspond to anything.')
+
+
+if __name__ == '__main__':
+    test = da.random.randint(0, 10, (200, 20, 20), chunks = (1, 20, 20))
+    filtered = spatial_filter(test, 2)
