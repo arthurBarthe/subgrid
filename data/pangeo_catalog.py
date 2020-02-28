311427da65009e7ae9f6d099abe4555128bdf1af
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-02-28 12:26:00 -0500

    Incorporate the grid cell area in the weights

--- data/pangeo_catalog.py
+++ data/pangeo_catalog.py
@@ -22,18 +22,28 @@ def get_patch(catalog_url, ntimes : int = None, bounds : list = None,
         s = catalog.ocean.GFDL_CM2_6.GFDL_CM2_6_control_ocean_surface
     else:
         raise('Only control implemented for now.')
+    s_grid = catalog.ocean.GFDL_CM2_6.GFDL_CM2_6_grid
     my_data = s.to_dask()
+    grid_data = s_grid.to_dask()
     if bounds is not None:
         my_data = my_data.sel(xu_ocean=slice(*bounds[2:]),
                            yu_ocean=slice(*bounds[:2]))
+        grid_data = grid_data.sel(xu_ocean=slice(*bounds[2:]),
+                                  yu_ocean=slice(*bounds[2:]))
     if ntimes is not None:
         my_data = my_data.isel(time=slice(0, ntimes))
     
     if selected_vars is None:
-        return my_data
+        return my_data, grid_data
     else:
-        return my_data[list(selected_vars)]
+        return my_data[list(selected_vars)], grid_data
 
 def get_whole_data():
     data = get_patch(catalog_url, None, None, 0, 'usurf', 'vsurf')
     return data
+
+if __name__ == '__main__':
+    import os
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/arthur/\
+access_key.json"
+    
