79d78720521d12d247fbaca563cbe83219e69929
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-03-18 09:29:23 -0400

    Removed time indices

--- testing/main.py
+++ testing/main.py
@@ -32,7 +32,8 @@ model_run = select_run(sort_by=cols[0], cols=cols[1:],
 
 # Load some extra parameters of the model.
 time_indices = model_run.time_indices
-test_split = model_run.test_split
+# TODO add back time indices
+# test_split = model_run.test_split
 batch_size = model_run.batch_size
 
 # Load the model's file
