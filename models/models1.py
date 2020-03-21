d9019574b426c38b43cc919f3c2d5ec2892e799f
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-03-21 18:47:27 -0400

    Increased size of LocallyConnected layer

--- models/models1.py
+++ models/models1.py
@@ -360,7 +360,7 @@ class Model3(MLFlowNN):
         self.add_conv2d_layer(32, 16, 3, padding=1)
         self.add_batch_norm_layer(16)
         self.add_locally_connected2d(self.height, self.width, 16, 2, 
-                                     kernel_size=15, padding=7,
+                                     kernel_size=31, padding=15,
                                      do_not_load=True)
         self.add_final_activation('identity')
 
