4a9c94b84ad93f78af5a3d7a565e2818f19c8b55
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-03-21 22:54:01 -0400

    New hyperparameters

--- models/models1.py
+++ models/models1.py
@@ -357,9 +357,9 @@ class Model3(MLFlowNN):
         self.add_conv2d_layer(32, 32, 3, padding=1)
         self.add_activation('relu')
         self.add_batch_norm_layer(32)
-        self.add_conv2d_layer(32, 8, 3, padding=1)
-        self.add_batch_norm_layer(8)
-        self.add_locally_connected2d(self.height, self.width, 8, 2, 
+        self.add_conv2d_layer(32, 32, 3, padding=1)
+        self.add_batch_norm_layer(32)
+        self.add_locally_connected2d(self.height, self.width, 32, 2, 
                                      kernel_size=5, padding=2,
                                      do_not_load=True)
         self.add_final_activation('identity')
