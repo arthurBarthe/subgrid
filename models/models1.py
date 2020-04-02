dea39544299687c6ea5c5dfad5a50937b7ee69ef
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-04-02 19:15:51 -0400

    Reducing kernel size of Locally Connected layer to 5

--- models/models1.py
+++ models/models1.py
@@ -361,7 +361,7 @@ class Model3(MLFlowNN):
         self.add_conv2d_layer(32, 32, 3, padding=1)
         self.add_batch_norm_layer(32)
         self.add_locally_connected2d(self.height, self.width, 32, 2, 
-                                     kernel_size=15, padding=7,
+                                     kernel_size=5, padding=2,
                                      do_not_load=True)
         self.add_final_activation('identity')
 
