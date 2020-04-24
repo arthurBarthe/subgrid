e4ef18f02e294fd1ee54dc027ad5764ea84df705
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-04-24 16:44:05 -0400

    Back to former way of calculating the precision

--- train/losses.py
+++ train/losses.py
@@ -8,20 +8,22 @@ In this module we define custom loss functions
 """
 import torch
 from torch.nn.modules.loss import _Loss
-from torch.distributions.normal import Normal
+
 
 class HeteroskedasticGaussianLoss(_Loss):
     def __init__(self, size_average=None, reduce=None, reduction='mean'):
-        super(HeteroskedasticGaussianLoss, self).__init__(size_average, 
-                                                          reduce, 
-                                                          reduction)
+        super(HeteroskedasticGaussianLoss, self).__init__(size_average,
+                                                          reduce, reduction)
 
-    def forward(self, input : torch.Tensor, target : torch.Tensor):
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
         # Split the target into mean (first half of channels) and scale
         mean, precision = torch.split(target, 2, dim=1)
-        # precision = torch.log(1 + torch.exp(precision)) + 0.01
-        precision = precision**2 + 1e-3
-        term1 = -torch.log(precision)
-        term2 = 1 / 2 * (input - mean)**2 * precision**2 
+        precision_ = torch.log(1 + torch.exp(precision)) + 0.01
+        # precision = precision**2 + 1e-3
+        if not torch.all(precision_ > 0):
+            raise ValueError('Got a non-positive precision value \
+                             Pre-processed precision tensor was: \
+                                 {}'.format(precision_))
+        term1 = -torch.log(precision_)
+        term2 = 1 / 2 * (input - mean)**2 * precision_**2
         return (term1 + term2).mean()
-
