a262247bc376bc184246813b5e6b18aef3529395
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-05-08 10:09:27 -0400

    Fitting dataset transformer to training data

--- testing/main.py
+++ testing/main.py
@@ -129,7 +129,7 @@ test_index = int(test_split * len(dataset))
 train_dataset = Subset(dataset, np.arange(train_index))
 test_dataset = Subset(dataset, np.arange(test_index, len(dataset)))
 
-train_dataset = dataset_transformer.transform(train_dataset)
+train_dataset = dataset_transformer.fit_transform(train_dataset)
 test_dataset = dataset_transformer.transform(test_dataset)
 
 # TODO Allow multiple time indices.
