aac75892cb7ca1485632fa8f9b9a7004a1ea4dae
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-05-08 09:08:32 -0400

    New loss (new definition of precision)

--- train/losses.py
+++ train/losses.py
@@ -21,3 +21,16 @@ class HeteroskedasticGaussianLoss(_Loss):
         term1 = - 1 / 2 * torch.log(precision)
         term2 = 1 / 2 * (input - mean)**2 * precision
         return (term1 + term2).mean()
+
+
+class HeteroskedasticGaussianLossV2(_Loss):
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
+        # Split the target into mean (first half of channels) and scale
+        mean, precision = torch.split(target, 2, dim=1)
+        if not torch.all(precision > 0):
+            raise ValueError('Got a non-positive variance value. \
+                             Pre-processed variance tensor was: \
+                                 {}'.format(torch.min(precision)))
+        term1 = - torch.log(precision)
+        term2 = 1 / 2 * (input - mean)**2 * precision**2
+        return (term1 + term2).mean()
