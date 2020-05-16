13640569aeec1b9acf7f6ebb8b0c6acb39b6d126
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-05-16 12:41:37 -0400

    Adding batchnorm

--- models/Unet.py
+++ models/Unet.py
@@ -18,7 +18,7 @@ import numpy as np
 
 class Unet(Module):
     def __init__(self, n_in_channels: int = 2, n_out_channels: int = 4,
-                 height=0, width=0, n_scales: int = 2):
+                 height=0, width=0, n_scales: int = 2, batch_norm=True):
         super().__init__()
         self.n_in_channels = n_in_channels
         self.n_out_channels = n_out_channels
@@ -31,6 +31,7 @@ class Unet(Module):
         self.linear_layer = None
         self._build_convs()
         self.linear_layer = None
+        self.batch_norm = batch_norm
 
     @property
     def transformation(self):
@@ -61,6 +62,12 @@ class Unet(Module):
     def up(self, x, i):
         return self.up_samplers[i](x)
 
+    def _make_subblock(self, conv):
+        subbloc = [conv, nn.ReLU()]
+        if self.batch_norm:
+            subbloc.append(nn.BatchNorm2d(conv.out_channels))
+        return subbloc
+
     def _build_convs(self):
         for i in range(self.n_scales):
             if i == 0:
@@ -71,7 +78,9 @@ class Unet(Module):
                 n_out_channels = 2 * n_out_channels
             conv1 = torch.nn.Conv2d(n_in_channels, n_out_channels, 3, padding=1)
             conv2 = torch.nn.Conv2d(n_out_channels, n_out_channels, 3, padding=1)
-            submodule = Sequential(conv1, nn.ReLU(), conv2, nn.ReLU())
+            block1 = self._make_subblock(conv1)
+            block2 = self._make_subblock(conv2)
+            submodule = Sequential(*block1, *block2)
             self.down_convs.append(submodule)
             self.conv_layers.append(conv1)
             self.conv_layers.append(conv2)
@@ -85,7 +94,9 @@ class Unet(Module):
             n_out_channels = n_out_channels // 2
             conv1 = torch.nn.Conv2d(n_in_channels, n_out_channels, 3, padding=1)
             conv2 = torch.nn.Conv2d(n_out_channels, n_out_channels, 3, padding=1)
-            submodule = Sequential(conv1, nn.ReLU(), conv2, nn.ReLU())
+            block1 = self._make_subblock(conv1)
+            block2 = self._make_subblock(conv2)
+            submodule = Sequential(*block1, *block2)
             self.up_convs.append(submodule)
             self.conv_layers.append(conv1)
             self.conv_layers.append(conv2)
