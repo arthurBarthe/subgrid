0547cfacdd2148dc190b3f7bc91c5956612ac8f5
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-05-28 12:43:19 -0400

    small fix for CLI args

--- cmip26.py
+++ cmip26.py
@@ -25,9 +25,9 @@ DESCRIPTION = 'Read data from the CMIP2.6 from a particular region and \
 
 # Parse the command-line parameters
 parser = argparse.ArgumentParser(description=DESCRIPTION)
-parser.add_argument('scale', type=float, default=30, help='scale in\
+parser.add_argument('--scale', type=float, default=32.6, help='scale in\
                     kilometers')
-parser.add_argument('bounds', type=float, nargs=4,
+parser.add_argument('--bounds', type=float, nargs=4,
                     default=[20, 35, -30, -15], help='min lat, max_lat,\
                     min_long, max_long')
 parser.add_argument('--ntimes', type=int, default=100, help='number of days,\
