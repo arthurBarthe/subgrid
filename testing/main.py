ec444dfd81f5cb92f228859868cd95cd15a9bb71
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-06-09 18:34:27 -0400

    Testing on multiple data

--- testing/main.py
+++ testing/main.py
@@ -24,7 +24,11 @@ from data.datasets import (RawDataFromXrDataset, DatasetTransformer,
 from train.base import Trainer
 from train.losses import (HeteroskedasticGaussianLoss, 
                           HeteroskedasticGaussianLossV2)
-from models.transforms import SoftPlusTransform
+
+from testing.utils import create_test_dataset
+
+from models.utils import load_model_cls
+
 import os.path
 import importlib
 
@@ -78,19 +82,19 @@ learning_rate = learning_rates[0] * lr_ratio
 client = mlflow.tracking.MlflowClient()
 model_file = client.download_artifacts(model_run.run_id,
                                        'models/trained_model.pth')
-# transformation_file = client.download_artifacts(model_run.run_id,
-#                                                 'models/transformation')
+transformation_file = client.download_artifacts(model_run.run_id,
+                                                'models/transformation')
 features_transform_file = client.download_artifacts(model_run.run_id,
                                                     'models/features_transform')
 try:
     targets_transform_file = client.download_artifacts(model_run.run_id,
-                                                    'models/targets_transform')
+                                                       'models/targets_transform')
 except FileNotFoundError:
     targets_transform_file = None
 
-# with torch.no_grad():
-#     with open(transformation_file, 'rb') as f:
-#         transformation = pickle.load(f)
+with torch.no_grad():
+    with open(transformation_file, 'rb') as f:
+        transformation = pickle.load(f)
 with open(features_transform_file, 'rb') as f:
     features_transform = pickle.load(f)
 if targets_transform_file is not None:
@@ -99,185 +103,119 @@ if targets_transform_file is not None:
 else:
     targets_transform = None
 
-
-
 # Prompt user to select the test dataset
 mlflow.set_experiment('forcingdata')
 cols = ['params.lat_min', 'params.lat_max', 'params.long_min',
         'params.long_max', 'params.scale']
-data_run = select_run(cols=cols)
-# TODO check that the run_id is different from source_data_id
-client = mlflow.tracking.MlflowClient()
-data_file = client.download_artifacts(data_run.run_id, 'forcing')
 
-# Set the experiment to 'multiscale'
-print('Logging to experiment multiscale')
-mlflow.set_experiment('multiregion')
-mlflow.start_run()
+i_test = 0
+while True:
+    i_test += 1
+    data_run = select_run(cols=cols)
+    if isinstance(data_run, int):
+        break
+    # Recover the data (velocities and forcing)
+    client = mlflow.tracking.MlflowClient()
+    data_file = client.download_artifacts(data_run.run_id, 'forcing')
 
-# Log the run_id of the loaded model (useful to recover info
-# about the scale that was used to train this model for
-# instance.
-mlflow.log_param('model_run_id', model_run.run_id)
-# Log the run_id for the data
-mlflow.log_param('data_run_id', data_run.run_id)
-mlflow.log_param('n_epochs', n_epochs)
-
-# Read the dataset file
-xr_dataset = xr.open_zarr(data_file).load()
-
-# To PyTorch Dataset
-dataset = RawDataFromXrDataset(xr_dataset)
-dataset.index = 'time'
-dataset.add_input('usurf')
-dataset.add_input('vsurf')
-dataset.add_output('S_x')
-dataset.add_output('S_y')
-
-train_index = int(train_split * len(dataset))
-test_index = int(test_split * len(dataset))
-train_dataset = Subset_(dataset, np.arange(train_index))
-
-if targets_transform is None:
-    transform = DatasetTransformer(features_transform)
-else:
-    transform = DatasetTransformer(features_transform, targets_transform)
-transform.fit(train_dataset)
-dataset = DatasetWithTransform(dataset, transform)
-dataset = MultipleTimeIndices(dataset)
-dataset.time_indices = [0, ]
-train_dataset = Subset_(dataset, np.arange(train_index))
-test_dataset = Subset_(dataset, np.arange(test_index, len(dataset)))
-
-# TODO Allow multiple time indices.
-# test_dataset = MultipleTimeIndices(test_dataset)
-test_dataset.time_indices = time_indices
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
-                              shuffle=True, drop_last=True)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
-                             shuffle=False, drop_last=True)
-print('Size of training data: {}'.format(len(train_dataset)))
-print('Size of validation data : {}'.format(len(test_dataset)))
-print('Height: {}'.format(train_dataset.height))
-print('Width: {}'.format(train_dataset.width))
-print(train_dataset[0][0].shape)
-print(train_dataset[0][1].shape)
-print(transform.transforms['features'].transforms)
-print(transform.transforms['targets'].transforms)
-
-
-# Load the model itself
-logging.info('Creating the neural network model')
-try:
-    module = importlib.import_module(model_module_name)
-    model_cls = getattr(module, model_cls_name)
-except ModuleNotFoundError as e:
-    raise type(e)('Could not retrieve the module in which the trained model \
-                  is defined: ' + str(e))
-except AttributeError as e:
-    raise type(e)('Could not retrieve the model\'s class. ' + str(e))
-
-net = model_cls(dataset.n_features, 2*dataset.n_targets)
-
-logging.info('Loading the neural net parameters')
-# Load parameters of pre-trained model
-net.final_transformation = SoftPlusTransform()
-net.load_state_dict(torch.load(model_file))
-
-
-
-train_dataset.add_targets_transform_from_model(net)
-test_dataset.add_targets_transform_from_model(net)
-
-net.to(device)
-
-# Set up training criterion and select parameters to train
-try:
-    criterion = getattr(modules['__main__'], loss_cls_name)()
-except AttributeError as e:
-    raise type(e)('Could not find the loss class used for training.')
-
-
-print('width: {}, height: {}'.format(dataset.width, dataset.height))
-# If the model has defined a linear layer we train that only. Otherwise
-# we train all the parameters for now
-# if net.linear_layer is not None:
-#     print('Training final layer only')
-#     parameters = net.linear_layer.parameters()
-# else:
-trainer = Trainer(net, device)
-trainer.criterion = criterion
-if n_epochs > 0:
-    print('Fine-tuning whole network')
-    parameters = net.parameters()
-    optimizer = torch.optim.Adam(parameters, lr=learning_rate, weight_decay=0)
+    # Set the experiment to 'multiregion'
+    print('Logging to experiment multiregion')
+    mlflow.set_experiment('multiregion')
+    mlflow.start_run()
+    mlflow.log_param('model_run_id', model_run.run_id)
+    mlflow.log_param('data_run_id', data_run.run_id)
+    mlflow.log_param('n_epochs', n_epochs)
     
+    # Read the dataset file
+    xr_dataset = xr.open_zarr(data_file).load()
+    
+    # To PyTorch Dataset
+    dataset = RawDataFromXrDataset(xr_dataset)
+    dataset.index = 'time'
+    dataset.add_input('usurf')
+    dataset.add_input('vsurf')
+    dataset.add_output('S_x')
+    dataset.add_output('S_y')
 
-# Training itself
-for i_epoch in range(n_epochs):
-    train_loss = trainer.train_for_one_epoch(train_dataloader, optimizer)
-    test_loss, metrics_results = trainer.test(test_dataloader)
-    print('Epoch {}'.format(i_epoch))
-    print('Train loss for this epoch is {}'.format(train_loss))
-    print('Test loss for this epoch is {}'.format(test_loss))
-    mlflow.log_metric('train loss', train_loss, i_epoch)
-    mlflow.log_metric('test loss', test_loss, i_epoch)
+    train_index = int(train_split * len(dataset))
+    test_index = int(test_split * len(dataset))
+    train_dataset = Subset_(dataset, np.arange(train_index))
 
-# Final test
-train_loss, train_metrics_results = trainer.test(train_dataloader)
-test_loss, test_metrics_results = trainer.test(test_dataloader)
-print(f'Final train loss is {train_loss}')
-print(f'Final test loss is {test_loss}')
+    transform = DatasetTransformer(features_transform, targets_transform)
+    transform.fit(train_dataset)
+    dataset = DatasetWithTransform(dataset, transform)
+    dataset = MultipleTimeIndices(dataset)
+    dataset.time_indices = [0, ]
+    train_dataset = Subset_(dataset, np.arange(train_index))
+    test_dataset = Subset_(dataset, np.arange(test_index, len(dataset)))
 
-# Do the predictions for that dataset using the loaded model
-velocities = np.zeros((len(test_dataset), 2, test_dataset.height,
-                       test_dataset.width))
-predictions = np.zeros((len(test_dataset), 4, test_dataset.output_height,
-                        test_dataset.output_width))
-truth = np.zeros((len(test_dataset), 2, test_dataset.output_height,
-                  test_dataset.output_width))
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                                  shuffle=True, drop_last=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size,
+                                 shuffle=False, drop_last=True)
+    print('Size of training data: {}'.format(len(train_dataset)))
+    print('Size of validation data : {}'.format(len(test_dataset)))
+    print('Height: {}'.format(train_dataset.height))
+    print('Width: {}'.format(train_dataset.width))
+    print(train_dataset[0][0].shape)
+    print(train_dataset[0][1].shape)
+    print('Features transform: ', transform.transforms['features'].transforms)
+    print('Targets transform: ', transform.transforms['targets'].transforms)
 
-net.eval()
-with torch.no_grad():
-    for i, data in enumerate(test_dataloader):
-        print(i)
-        uv_data = data[0][:, :2, ...].numpy()
-        velocities[i * batch_size: (i + 1) * batch_size] = uv_data
-        truth[i * batch_size: (i + 1) * batch_size] = data[1].numpy()
-        X = data[0].to(device, dtype=torch.float)
-        pred_i = net(X)
-        pred_i = pred_i.cpu().numpy()
-        predictions[i * batch_size: (i+1) * batch_size] = pred_i
+    # Load the model itself
+    logging.info('Creating the neural network model')
+    model_cls = load_model_cls(model_module_name, model_cls_name)
+    net = model_cls(dataset.n_features, 2*dataset.n_targets)
 
-# Put this into an xarray dataset before saving
-new_dims = ('time', 'latitude', 'longitude')
-coords = xr_dataset.coords
-coords_uv = {'time': coords['time']
-                  [test_index:test_index+len(test_dataset)],
-                  'latitude': coords['yu_ocean'].data[:test_dataset.height],
-                  'longitude': coords['xu_ocean'].data[:test_dataset.width]}
-coords_s = {'time': coords['time']
-              [test_index:test_index+len(test_dataset)],
-              'latitude': coords['yu_ocean'].data[:test_dataset.output_height],
-              'longitude': coords['xu_ocean'].data[:test_dataset.output_width]}
-u_surf = xr.DataArray(data=velocities[:, 0, ...], dims=new_dims,
-                      coords=coords_uv)
-v_surf = xr.DataArray(data=velocities[:, 1, ...], dims=new_dims,
-                      coords=coords_uv)
-s_x = xr.DataArray(data=truth[:, 0, ...], dims=new_dims, coords=coords_s)
-s_y = xr.DataArray(data=truth[:, 1, ...], dims=new_dims, coords=coords_s)
-s_x_pred = xr.DataArray(data=predictions[:, 0, ...], dims=new_dims,
-                        coords=coords_s)
-s_y_pred = xr.DataArray(data=predictions[:, 1, ...], dims=new_dims,
-                        coords=coords_s)
-s_x_pred_scale = xr.DataArray(data=predictions[:, 2, ...], dims=new_dims,
-                              coords=coords_s)
-s_y_pred_scale = xr.DataArray(data=predictions[:, 3, ...], dims=new_dims,
-                              coords=coords_s)
-output_dataset = xr.Dataset({'u_surf': u_surf, 'v_surf': v_surf,
-                             'S_x': s_x, 'S_y': s_y, 'S_xpred': s_x_pred,
-                             'S_ypred': s_y_pred, 'S_xscale': s_x_pred_scale,
-                             'S_yscale': s_y_pred_scale})
+    logging.info('Loading the neural net parameters')
+    # Load parameters of pre-trained model
+    net.final_transformation = transformation
+    net.load_state_dict(torch.load(model_file))
+
+    # Adding transforms required by the model
+    dataset.add_targets_transform_from_model(net)
+
+    # Net to GPU
+    net.to(device)
+
+    # Set up training criterion and select parameters to train
+    try:
+        criterion = getattr(modules['__main__'], loss_cls_name)()
+    except AttributeError as e:
+        raise type(e)('Could not find the loss class used for training.')
+
+    print('width: {}, height: {}'.format(dataset.width, dataset.height))
+
+    trainer = Trainer(net, device)
+    trainer.criterion = criterion
+    if n_epochs > 0:
+        print('Fine-tuning whole network')
+        parameters = net.parameters()
+        optimizer = torch.optim.Adam(parameters, lr=learning_rate)
+
+    # Training itself
+    for i_epoch in range(n_epochs):
+        train_loss = trainer.train_for_one_epoch(train_dataloader, optimizer)
+        test_loss, metrics_results = trainer.test(test_dataloader)
+        print('Epoch {}'.format(i_epoch))
+        print('Train loss for this epoch is {}'.format(train_loss))
+        print('Test loss for this epoch is {}'.format(test_loss))
+        mlflow.log_metric('train loss', train_loss, i_epoch)
+        mlflow.log_metric('test loss', test_loss, i_epoch)
+
+    # Final test
+    train_loss, train_metrics_results = trainer.test(train_dataloader)
+    test_loss, test_metrics_results = trainer.test(test_dataloader)
+    print(f'Final train loss is {train_loss}')
+    print(f'Final test loss is {test_loss}')
+
+    # Do the predictions for that dataset using the loaded model
+    out = create_test_dataset(net, xr_dataset, test_dataset,
+                              test_dataloader, test_index, device)
+    if i_test == 1:
+        output_dataset = out
+    else:
+        output_dataset = output_dataset.merge(out)
 
 # Save dataset
 file_path = os.path.join(data_location, 'test_output')
