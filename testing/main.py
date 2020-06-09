b6058520120d014424e5b5f0ccd4dd0240b4052b
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-06-09 18:47:37 -0400

    Allowing to select models from various experiments

--- testing/main.py
+++ testing/main.py
@@ -45,9 +45,11 @@ from sys import modules
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=0)
 parser.add_argument('--lr_ratio', type=float, default=1)
+parser.add_argument('--models_experiment_name', type=str, default='training')
 script_params = parser.parse_args()
 n_epochs = script_params.n_epochs
 lr_ratio = script_params.lr_ratio
+models_experiment_name = script_params.models_experiment_name
 
 # Location used to write generated data before it is logged through MLFlow
 data_location = tempfile.mkdtemp(dir='/scratch/ag7531/temp/')
@@ -60,8 +62,11 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # experiment (folder mlruns/0)
 cols = ['metrics.test mse', 'start_time', 'params.time_indices',
         'params.model_cls_name', 'params.source.run_id']
+# Recover experiment id of the models
+models_experiment = mlflow.get_experiment_by_name(models_experiment_name)
+models_experiment_id = models_experiment.experiment_id
 model_run = select_run(sort_by='start_time', cols=cols,
-                       experiment_ids=['2', ])
+                       experiment_ids=[models_experiment_id, ])
 
 # Load some extra parameters of the model.
 # TODO allow general time_indices
