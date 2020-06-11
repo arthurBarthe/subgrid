30a4f4867d435344c0e9eb70dcf5ed5a093e059a
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-06-11 13:19:53 -0400

    Added function to merge errors

--- analysis/base.py
+++ analysis/base.py
@@ -7,6 +7,7 @@ Created on Thu Jun 11 11:13:35 2020
 """
 import numpy as np
 import xarray as xr
+import mlflow
 
 class TestDataset:
     def __init__(ds):
@@ -27,4 +28,24 @@ class TestDataset:
         errors = self.errors(normalized)
         return np.sqrt((errors['S_x']**2 + errors['S_y']**2).mean(dim=dim))
 
+def get_test_datasets(run_id: str):
+    """Return a list of the test datasets for the provided run id"""
+    client = mlflow.tracking.MlflowClient()
+    artifacts = client.list_artifacts(run_id)
+    test_outputs = list()
+    for a in artifacts:
+        if a.startswith('test_ouput_'):
+            ds = xr.open_zarr(client.download_artifacts(run_id, a))
+            test_outputs.append(TestDataset(ds))
+    return test_outputs
+
+def get_merged_errors(test_datasets, error_func, dim, *args):
+    """Compute the summary errors for a list of test datasets and merge those
+    errors"""
+    errors = list()
+    for i, ds in enumerate(test_datasets):
+        error = getattr(ds, error_func)(dim, *args)
+        error.name = error_func
+        errors.append(error)
+    return xr.merge(errors)
     
