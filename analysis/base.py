ebdce04909a1b8e22e2f0b6c9678c94e119f704a
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-06-11 14:52:55 -0400

    minor fix

--- analysis/base.py
+++ analysis/base.py
@@ -36,7 +36,9 @@ def get_test_datasets(run_id: str):
     test_outputs = list()
     for a in artifacts:
         basename = os.path.basename(a.path)
+        print(basename)
         if basename.startswith('test_ouput_'):
+            print('loading')
             ds = xr.open_zarr(client.download_artifacts(run_id, basename))
             test_outputs.append(TestDataset(ds))
     return test_outputs
