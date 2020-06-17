4275fcbc0858063e1b2a526e90e890e960890b2f
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-06-17 11:18:31 +0200

    New metric: max error

--- testing/metrics.py
+++ testing/metrics.py
@@ -9,10 +9,14 @@ the efficiency of models. These metrics classes allow to define an inverse
 transform that ensures that the metric is calculated independently of the
 normalization applied.
 """
+
+import numpy as np
+import torch
 from torch.nn.functional import mse_loss
+from collections.abc import ABC, abstractmethod
 
 
-class Metric:
+class Metric(ABC):
     def __init__(self, metric_func, name: str = None):
         if name is None:
             self.name = self.__class__.name
@@ -50,6 +54,19 @@ class Metric:
     def __call__(self, y_hat, y):
         return self.func(y_hat, y)
 
+    @abstractmethod
+    def update(self, y_hat, y):
+        pass
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+
+class MSEMetric(Metric):
+    def __init__(self):
+        super(MSEMetric, self).__init__(mse_loss)
+
     def update(self, y_hat, y):
         value = self(y_hat, y)
         value = value.item()
@@ -61,6 +78,17 @@ class Metric:
         self.i_batch = 0
 
 
-class MSEMetric(Metric):
+class MaxMetric(Metric):
     def __init__(self):
-        super(MSEMetric, self).__init__(mse_loss)
+        def func(x):
+            return torch.max(torch.abs(x))
+        super(MaxMetric, self).__init__(func)
+
+    def update(self, y_hat, y):
+        value = self(y_hat, y)
+        value = value.item()
+        self.value = np.max(value, self.value)
+
+    def reset(self):
+        self.value = 0
+        self.i_batch = 0
