13940734d48157edf70de4d31a8bee536fc8b511
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-07-22 09:29:54 +0200

    Adding high-rez filtered surface velocities

--- data/coarse.py
+++ data/coarse.py
@@ -187,6 +187,8 @@ def eddy_forcing(u_v_dataset, grid_data, scale: float, method: str = 'mean',
                                             'adv_y': 'f_adv_y'})
         adv_filtered = adv_filtered.rename({'adv_x': 'adv_f_x',
                                             'adv_y': 'adv_f_y'})
-        u_v_dataset = xr.merge((u_v_dataset, adv, filtered_adv, adv_filtered,
-                                forcing[['S_x', 'S_y']]))
+        u_v_filtered = u_v_filtered.rename({'usurf': 'f_usurf',
+                                            'vsurf': 'f_usurf'})
+        u_v_dataset = xr.merge((u_v_dataset, u_v_filtered, adv, filtered_adv,
+                                adv_filtered, forcing[['S_x', 'S_y']]))
         return u_v_dataset, forcing_coarse
