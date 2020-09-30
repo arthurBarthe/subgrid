56a1f0cc89dd496445434c73324769ef0849f7cc
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-09-30 22:25:38 +0200

    logging mode to info

--- cmip26.py
+++ cmip26.py
@@ -23,7 +23,7 @@ import tempfile
 from os.path import join
 
 # logging config
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
