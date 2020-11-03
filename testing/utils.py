c40d80bd4170549317918b767f3b6a2856bd9604
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-11-03 10:44:00 -0500

    modify dask computations for performance improvement

--- testing/utils.py
+++ testing/utils.py
@@ -122,10 +122,9 @@ def create_large_test_dataset(net, test_datasets, test_loaders, device,
         test_dataset = test_datasets[i]
         delayed_apply = dask.delayed(apply_net)
         temp = delayed_apply(net, loader, device)
-        shape = (loader.batch_size, 4, test_dataset.output_height,
+        shape = (len(loader), 4, test_dataset.output_height,
                  test_dataset.output_width)
-        output = [da.from_delayed(temp[0][i], shape=shape, dtype=np.float64)
-                  for i in range(len(loader))]
+        output = da.from_delayed(temp[0], shape=shape, dtype=np.float64)
         output = da.concatenate(output)
         # Same for input
         if save_input:
