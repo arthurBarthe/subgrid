e545e87ad0c25677c79eeff1003dbda2f709d46d
Arthur Guillaumin <arthur.guillaumin@gmail.com>
2020-11-06 16:04:30 -0500

    Use of submodels

--- testing/main.py
+++ testing/main.py
@@ -113,7 +113,7 @@ model_cls_name = model_run['params.model_cls_name']
 loss_cls_name = model_run['params.loss_cls_name']
 learning_rates = learning_rates_from_string(model_run['params.learning_rate'])
 weight_decay = float(model_run['params.weight_decay'])
-submodel_name = model_run['submodel']
+submodel_name = model_run['params.submodel']
 
 learning_rate = learning_rates[0] * lr_ratio
 
