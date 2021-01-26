a1bb68c016d1b10eda4e9e5278763dd55646c85b
Arthur <arthur.guillaumin@gmail.com>
2021-01-25 20:40:33 -0500

    Updates to use latest versions of intake, fsspec

--- data/pangeo_catalog.py
+++ data/pangeo_catalog.py
@@ -56,10 +56,7 @@ def get_patch(catalog_url, ntimes: int = None, bounds: list = None,
     s_grid = catalog.ocean.GFDL_CM2_6.GFDL_CM2_6_grid
     # The following lines are for caching
     source.urlpath = 'filecache::' + source.urlpath
-    s_grid.urlpath = 'filecache::' + source.urlpath
     source.storage_options['filecache'] = dict(
-        cache_storage=cache_folder)
-    s_grid.storage_options['filecache'] = dict(
         cache_storage=cache_folder)
     # Convert to dask
     uv_data = source.to_dask()
